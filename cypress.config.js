e0e99111f13a58b52e2e40ff8c1d14ed13bb383b
WGalvao <wagnerjbgalvao@gmail.com>
2025-02-11 12:32:49 -0300

    Aula 007 - Acessar a pagina Central de Atendimento ao Cliente TAT

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,7 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
-})
+});
--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Central de Atendimento ao Cliente TAT", () => {
+  it("Acessar a pagina Central de Atendimento ao Cliente TAT", () => {
+    cy.visit("./src/index.html");
+    cy.title().should("equal", "Central de Atendimento ao Cliente TAT");
+  });
+});
